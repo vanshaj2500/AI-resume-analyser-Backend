3b7b4a75286040ff2f0146a72bec19ac66d5f365
Vanshaj Sharma <122958071+vanshaj2500@users.noreply.github.com>
2026-03-28 03:50:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="AI Resume Analyzer", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://ai-resume-analyser-frontend-c2bz.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
